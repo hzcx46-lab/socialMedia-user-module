e08e6d654cca19c40cf186759bad37a8ea7bad5f
huangzichun <hzcx46@163.com>
2026-05-17 16:03:52 +0800

    feat: 初始化 socialMedia 用户domain模块项目框架

---
+++ socialMedia-user-module-domain/src/main/java/com/huangzichun/socialmediausermoduledomain/SocialMediaUserModuleDomainApplication.java
@@ -0,0 +1,13 @@
+package com.huangzichun.socialmediausermoduledomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaUserModuleDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaUserModuleDomainApplication.class, args);
+    }
+
+}
---
+++ socialMedia-user-module-domain/src/test/java/com/huangzichun/socialmediausermoduledomain/SocialMediaUserModuleDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangzichun.socialmediausermoduledomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaUserModuleDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

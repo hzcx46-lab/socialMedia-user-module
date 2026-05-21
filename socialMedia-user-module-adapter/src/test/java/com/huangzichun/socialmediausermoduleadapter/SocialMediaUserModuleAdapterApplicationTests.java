08a2b17ec2908d89f1dbb01cf494b5e4b912861b
huangzichun <hzcx46@163.com>
2026-05-21 20:53:51 +0800

    adapter层初始化完成

---
+++ socialMedia-user-module-adapter/src/test/java/com/huangzichun/socialmediausermoduleadapter/SocialMediaUserModuleAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangzichun.socialmediausermoduleadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaUserModuleAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

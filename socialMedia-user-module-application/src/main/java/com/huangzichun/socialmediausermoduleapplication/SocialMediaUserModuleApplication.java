6d4eeebc56a031bb104ee43851c78c3379665edd
huangzichun <hzcx46@163.com>
2026-05-21 20:49:26 +0800

    appliction层初始化架构完成

---
+++ socialMedia-user-module-application/src/main/java/com/huangzichun/socialmediausermoduleapplication/SocialMediaUserModuleApplication.java
@@ -0,0 +1,13 @@
+package com.huangzichun.socialmediausermoduleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaUserModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaUserModuleApplication.class, args);
+    }
+
+}
---
+++ socialMedia-user-module-application/src/test/java/com/huangzichun/socialmediausermoduleapplication/SocialMediaUserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangzichun.socialmediausermoduleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaUserModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

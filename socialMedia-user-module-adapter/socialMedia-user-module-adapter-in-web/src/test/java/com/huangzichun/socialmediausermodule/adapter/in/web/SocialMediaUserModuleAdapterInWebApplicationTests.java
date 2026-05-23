367e308e2eb63822b92d7cb7307b762c80f5e1f9
huangzichun <hzcx46@163.com>
2026-05-23 20:34:21 +0800

    in-web框架初始化

---
+++ socialMedia-user-module-adapter/socialMedia-user-module-adapter-in-web/src/test/java/com/huangzichun/socialmediausermodule/adapter/in/web/SocialMediaUserModuleAdapterInWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangzichun.socialmediausermodule.adapter.in.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaUserModuleAdapterInWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

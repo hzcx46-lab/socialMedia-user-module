62a6df4c34a458974e423f604136007aca6d6c72
huangzichun <hzcx46@163.com>
2026-05-27 21:18:39 +0800

    feat: 添用户资料命令对象

---
+++ socialMedia-user-module-application/src/main/java/com/huangzichun/socialmediausermoduleapplication/port/command/UserProfileCommand.java
@@ -0,0 +1,17 @@
+package com.huangzichun.socialmediausermoduleapplication.port.command;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public record UserProfileCommand(
+        Long id,
+        String nickname,
+        String avatar,
+        Integer gender,
+        LocalDate birthday,
+        String income,
+        String profession,
+        Integer marriage,
+        String coverPic,
+        Map<String, Object> tags) {
+}

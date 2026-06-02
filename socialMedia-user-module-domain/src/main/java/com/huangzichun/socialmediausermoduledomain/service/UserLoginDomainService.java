43ae1ab59515dd53a36ad295264e2507ae26640d
huangzichun <hzcx46@163.com>
2026-06-02 22:50:47 +0800

    实现用户登入服务的实体部分

---
+++ socialMedia-user-module-domain/src/main/java/com/huangzichun/socialmediausermoduledomain/service/UserLoginDomainService.java
@@ -0,0 +1,8 @@
+package com.huangzichun.socialmediausermoduledomain.service;
+
+import com.huangzichun.socialmediausermoduledomain.port.PasswordMatcher;
+
+public class UserLoginDomainService {
+    private final UserRepository userRepository;
+    private final PasswordMatcher passwordMatcher;
+}

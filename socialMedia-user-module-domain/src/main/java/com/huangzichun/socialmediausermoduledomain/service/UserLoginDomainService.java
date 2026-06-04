420a9d08aae62b7d68588235ed74e6bdd4873c65
huangzichun <hzcx46@163.com>
2026-06-04 21:57:01 +0800

    实现用户的手机登入方法

--- socialMedia-user-module-domain/src/main/java/com/huangzichun/socialmediausermoduledomain/service/UserLoginDomainService.java
+++ socialMedia-user-module-domain/src/main/java/com/huangzichun/socialmediausermoduledomain/service/UserLoginDomainService.java
@@ -1,5 +1,7 @@
 package com.huangzichun.socialmediausermoduledomain.service;
 
+import com.huangzichun.socialmediausermoduledomain.exception.DomainException;
+import com.huangzichun.socialmediausermoduledomain.model.User;
 import com.huangzichun.socialmediausermoduledomain.port.PasswordMatcher;
 import com.huangzichun.socialmediausermoduledomain.port.UserRepository;
 
@@ -10,5 +12,9 @@ public class UserLoginDomainService {
         this.userRepository = userRepository;
         this.passwordMatcher = passwordMatcher;
     }
+    public User loginByMobile(String mobile, String rawPassword) {
+        if (rawPassword == null || rawPassword.trim().isEmpty()) {
+            throw new DomainException(ResponseCodeEnum.USERNAME_OR_PWD_ERROR);
+        }
 
 }

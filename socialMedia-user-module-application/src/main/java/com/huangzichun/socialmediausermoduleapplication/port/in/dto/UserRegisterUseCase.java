441398f030e54be8e0ba5d47ef154121447884a1
huangzichun <hzcx46@163.com>
2026-06-14 16:35:33 +0800

    完成适配器框架搭建

--- socialMedia-user-module-application/src/main/java/com/huangzichun/socialmediausermoduleapplication/port/in/dto/UserRegisterUseCase.java
+++ socialMedia-user-module-application/src/main/java/com/huangzichun/socialmediausermoduleapplication/port/in/dto/UserRegisterUseCase.java
@@ -1,5 +1,7 @@
 package com.huangzichun.socialmediausermoduleapplication.port.in.dto;
 
+import com.huangzichun.socialmediausermoduleapplication.port.in.command.UserRegisterCommand;
+
 public interface UserRegisterUseCase {
     void register(UserRegisterCommand registerCommand);
 }

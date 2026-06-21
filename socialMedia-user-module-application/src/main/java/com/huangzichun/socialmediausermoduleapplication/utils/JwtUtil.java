d184e5973041d818b195186f5923b1066b590c02
huangzichun <hzcx46@163.com>
2026-06-21 23:35:47 +0800

    创建JWT工具

--- socialMedia-user-module-application/src/main/java/com/huangzichun/socialmediausermoduleapplication/utils/JwtUtil.java
+++ socialMedia-user-module-application/src/main/java/com/huangzichun/socialmediausermoduleapplication/utils/JwtUtil.java
@@ -1,17 +1,1 @@
-package com.huangzichun.socialmediausermoduleapplication.utils;
-
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class LoginApplicationService implements UserLoginUseCase {
-
-    private final UserLoginDomainService userLoginDomainService;
-    private final JwtUtil jwtUtil;
-
-    public LoginApplicationService(
-            UserLoginDomainService userLoginDomainService,
-            JwtUtil jwtUtil) {
-        this.userLoginDomainService = userLoginDomainService;
-        this.jwtUtil =jwtUtil;
-    }
+public class JwtUtil {
